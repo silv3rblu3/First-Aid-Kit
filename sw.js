e6d1a3f8cecc50274525bb849528fb2747a3ab6b
silv3rblu3 <silverblue.sheffer@gmail.com>
2026-04-24 15:29:52 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,60 +1,61 @@
-// 1. Define a name for your cache
-const CACHE_NAME = 'first-aid-app-v1';
+// sw.js
 
-// 2. List every single file your app needs to run offline
+const CACHE_NAME = 'omnihub-v1';
+
+// These are all the files the app needs to function completely offline.
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
-    './styles.css',
-    './script.js',
     './manifest.json',
-    './icon-192.png',
-    './icon-512.png'
+    './css/global.css',
+    './css/components.css',
+    './js/core.js',
+    './js/stateManager.js',
+    './js/notifications.js',
+    './apps/inventory/template.js',
+    './apps/inventory/app.js',
+    './apps/fleet/template.js',
+    './apps/fleet/app.js',
+    './apps/winterization/template.js',
+    './apps/winterization/app.js',
+    './apps/first-aid/template.js',
+    './apps/first-aid/app.js',
+    // Cache the external libraries for offline barcode scanning and CSV parsing
+    'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.4.1/papaparse.min.js',
+    'https://unpkg.com/html5-qrcode'
 ];
 
-// 3. The Install Event: This fires the very first time the browser sees this service worker.
+// Install Event: Caches all the files listed above
 self.addEventListener('install', (event) => {
-    // We tell the browser to wait until the caching is completely finished before considering the install successful.
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Opened cache');
-                // This downloads and saves all the files listed in the array above.
-                return cache.addAll(ASSETS_TO_CACHE);
-            })
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Opened cache');
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
     );
 });
 
-// 4. The Activate Event: This fires when the service worker starts up. 
-// It's used for cleanup, ensuring you don't have old versions of files taking up space.
+// Fetch Event: When offline, it serves files from the cache instead of the network
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            // Return the cached version if we have it, otherwise go to the network
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Activate Event: Clears out old caches if you ever update the CACHE_NAME version
 self.addEventListener('activate', (event) => {
     event.waitUntil(
         caches.keys().then((cacheNames) => {
             return Promise.all(
                 cacheNames.map((cacheName) => {
-                    // If the cache name doesn't match our current version, delete it.
                     if (cacheName !== CACHE_NAME) {
-                        console.log('Deleting old cache:', cacheName);
                         return caches.delete(cacheName);
                     }
                 })
             );
         })
-    );
-});
-
-// 5. The Fetch Event: This is the proxy. Every time your app asks for a file or an image, this event intercepts it.
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        // First, we check if the requested file is already saved in our cache.
-        caches.match(event.request)
-            .then((response) => {
-                // If it IS in the cache, we return the cached version immediately (offline support).
-                if (response) {
-                    return response;
-                }
-                // If it is NOT in the cache, we go out to the network and fetch it normally.
-                return fetch(event.request);
-            })
     );
 });
